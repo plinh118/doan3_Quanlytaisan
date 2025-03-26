9d1e68e14cf04bd0d3cc8c6c3883d7f30fe3a31c
unknown <jaykergg@gmail.com>
2025-03-26 21:45:45 +0700

    updatesssss

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   const pathname = usePathname()
   const [collapsed, setCollapsed] = useState(false)
 
-  const isAuthPage = pathname === "/vi" || pathname === "/vi/register" || pathname ==="/vi/home_user";
+  const isAuthPage = pathname === "/vi" || pathname === "/vi/register" || pathname ==="/vi/home_user" || pathname ==="/vi/login";
 
   const siderStyle: React.CSSProperties = {
     position: "fixed",
